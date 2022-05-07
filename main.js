29d8cd2d89c1d2f3d6b696332f2dd99de81c5534
Nikola Pavicevic <nijepa@gmail.com>
2022-05-07 09:45:05 +0200

    removed cards component

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import { defineCustomElement } from 'vue';
 import Toast from './components/Custom-Toast.ce.vue';
-
 const CustomToast = defineCustomElement(Toast);
-
 customElements.define('custom-toast', CustomToast);
